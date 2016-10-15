086ba84967f17317946548d2ca48c323c230fa76
Nahuelr <nahuelrossi.dev@gmail.com>
2016-10-15 12:12:50 -0300

    urls.py
    añadido el nombre a la url de login
    cambiado el orden de las urls

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views
+# from goalsettracker.forms import LoginForm
 
 urlpatterns = [
-    url(r'', include('goalsettracker.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^login/$', views.login, {'template_name': 'login.html'}),
+    url(r'', include('goalsettracker.urls')),
+    url(r'^login/$', views.login, {'template_name': 'login.html'}, name='login')  # , 'authentication_form': LoginForm}),
 ]
