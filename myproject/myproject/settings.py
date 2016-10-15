6df9cbd22d6bfb1750bb13c875e22a187dc28b55
Nahuelr <nahuelrossi.dev@gmail.com>
2016-10-15 11:17:41 -0300

    configurada la ubicacion de la carpeta templates

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, "static"),
+]
