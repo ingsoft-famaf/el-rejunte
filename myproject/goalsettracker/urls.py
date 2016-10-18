062d41abd2c66cba170fa60a958be24264b4c5ab
burrito1 <gon.rivero10@gmail.com>
2016-10-18 19:22:58 -0300

    Ya se registran los usuarios en la base de datos

--- myproject/goalsettracker/urls.py
+++ myproject/goalsettracker/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 from . import views
+from goalsettracker.views import Register
 
 
 # We are adding a URL called /home
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^home/$', views.home, name='home'),
-    url(r'^login/register$', views.register, name='register'),
-    url(r'^register/$', views.register, name = 'register'),
-    url(r'^register/success/$', views.register_success, name='register_success'),
+    url(r'^login/register$', Register.as_view(), name='register'),
+    url(r'^register/', Register.as_view(), name='register'),
+    #url(r'^register/$', views.register, name = 'register'),
 ]
--- myproject/goalsettracker/views.py
+++ myproject/goalsettracker/views.py
@@ -6,6 +6,8 @@ from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
+from forms import UserCreationForm
+from django.views.generic import CreateView
 
 # Create your views here.
 # this login required decorator is to not allow to any
@@ -13,35 +15,15 @@ from django.template import RequestContext
 def index(request):
     return render(request, "index.html")
 
-
-def register(request):
-    if request.method == 'POST':
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            user = User.objects.create_user(
-            username=form.cleaned_data['username'],
-            password=form.cleaned_data['password1'],
-            email=form.cleaned_data['email']
-            )
-            return HttpResponseRedirect('/register/success/')
-    else:
-        form = RegistrationForm()
-    variables = RequestContext(request, {
-    'form': form
-    })
- 
-    return render_to_response(
-    'registration/register.html',
-    variables,
-    )
- 
-def register_success(request):
-    return render_to_response(
-    'registration/success.html',
-    )
- 
-def logout_page(request):
-    logout(request)
+class Register(CreateView):
+    """
+    Vista de registro de usuario para uso de django. Posee la funcionalidad
+    de crear nuevos usuarios con sus passwords. Hereda de
+    django.views.generic.CreateView
+    """
+    template_name = 'registration/register.html'
+    form_class = UserRegisterForm
+    success_url = '/login/'
 
 @login_required
 def home(request):
